76ff4a783d6bacff9c0ea90abb54726cfc5c1e76
Andreas Kröpelin <andreas-kroepelin@gmx.de>
2025-06-27 22:44:41 +0200

    Start bringing this to modern idiomatic Julia

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,18 +7,18 @@ using Base.Test
 # Since the gauss transform is linear, this should be a sufficient test
 # of the accuracy of the algorithm in general.
 function maxerror(T)
-  m = zero(T)
-  for xp in linspace(convert(T, -10), convert(T, 10), 1000)
-   points = [zero(T), convert(T, 2), xp]
-   values = ones(T, size(points))
-   f = fastgausstransform(points, values, convert(T, 0.5))
-   s = slowgausstransform(points, values, convert(T, 0.5))
-   for x in linspace(convert(T, -10), convert(T, 10), 10000)
-     delta = abs(evaluate(f, x) - evaluate(s, x))
-     m = max(m, delta)
-   end
-  end
-  return m
+    m = zero(T)
+    for xp in linspace(convert(T, -10), convert(T, 10), 1000)
+        points = [zero(T), convert(T, 2), xp]
+        values = ones(T, size(points))
+        f = fastgausstransform(points, values, convert(T, 0.5))
+        s = slowgausstransform(points, values, convert(T, 0.5))
+        for x in linspace(convert(T, -10), convert(T, 10), 10000)
+            delta = abs(evaluate(f, x) - evaluate(s, x))
+            m = max(m, delta)
+        end
+    end
+    return m
 end
 
 # Factor of 4 is from 3 particles, rounded to nearest power of 2 for rounding error
